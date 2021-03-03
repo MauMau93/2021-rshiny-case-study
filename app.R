6751dd68ba2a2d5c53634405d1999f30159338b1
MauMau93 <mauriciomarcosf@gmail.com>
2021-03-03 15:08:59 +0100

    table of ggapminder dataset

---
+++ app.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+library(gapminder)
+
+dataPanel <- tabPanel("Data",
+                      tableOutput("data")
+)
+
+
+# Define UI for application that draws a histogram
+ui <- navbarPage("shiny App",
+                 dataPanel
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) { 
+  output$data <- renderTable(gapminder)
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
